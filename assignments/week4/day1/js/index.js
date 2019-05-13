4cb16d197988bc5cbfbcf6721c3fc595adac962d
sonutiwari <ankorha@gmail.com>
2019-05-13 15:12:18 +0530

    Final assignment week 4 day 1 submit

--- assignments/week4/day1/js/index.js
+++ assignments/week4/day1/js/index.js
@@ -1,10 +1,15 @@
 function printTable(){
     var N = document.getElementById("number").value;
-    var ans = "<br /><h3>Here is the table of " + N + "</h3> <br />";
-    for(let i = 1; i <= 10; i++){
-        ans += i.toString() + " * " + N.toString() + " = " + (N * i).toString() + "<br />";
+    if(N <= 0){
+        alert("Please enter a valid number, for multiplication table");
+    } else {
+        var ans = "<br /><h3>Here is the table of " + N + "</h3> <br />";
+        for(let i = 1; i <= 10; i++){
+            ans += i.toString() + " * " + N.toString() + " = " + (N * i).toString() + "<br />";
+        }
+        console.log(ans);
+        var div = document.getElementById("table_div");
+        div.innerHTML = div.innerHTML + ans;
+        div.style.display = "block";
     }
-    console.log(ans);
-    var div = document.getElementById("table_div");
-    div.innerHTML = div.innerHTML + ans;
 }
