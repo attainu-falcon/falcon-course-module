5a5f2d6905512214c0f6c4a6d0e7cf69b46ae6ee
shahrukh2893 <shahrukhanwer92@gmail.com>
2019-05-14 13:29:04 +0530

    Modified Multiplication Table

--- assignments/Multiplication Table/script.js
+++ assignments/Multiplication Table/script.js
@@ -2,7 +2,7 @@ document.querySelector('button').addEventListener('click', myFunction);
 
 function myFunction(){
   var val = document.querySelector('input').value
-  var res = "";
+  document.getElementById('result').innerHTML = ""
   for (var i=1; i<11; i++){
     document.getElementById('result').innerHTML += i + ' * '+ val + ' = ' + val * i +"<br>";
   }
