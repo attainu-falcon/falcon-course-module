ee882c82d98232527453d12c7544be8cf4e4d532
sidd <lovessp58@gmail.com>
2019-05-21 00:36:08 +0530

    new js file added in coding challenge folder

---
+++ coding challenges/printName.js
@@ -0,0 +1,16 @@
+function printInitials(name){
+	var i,initials;
+
+	intials = name.charAt(0);
+
+	for (i=1; i<name.length;i=i++){
+	if (name.charAt[i] == ' '){
+	initials = initials + name.charAt(i+1);
+	  }
+	}
+	console.log(initials);
+
+}
+
+printInitials("Mahendra Singh Dhoni");
+printInitials("shah Rukh Khan");
