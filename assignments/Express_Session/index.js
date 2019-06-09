1b8cca0b14439acdd8e2d175959a214d8affc3f1
Arkesh Jaiswal <arkesh@attainu.com>
2019-06-09 23:38:13 +0530

    protect, fs, multer

--- assignments/Express_Session/index.js
+++ assignments/Express_Session/index.js
@@ -1,10 +1,14 @@
 var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
+var fs = require('fs');
+var multer = require('multer');
+var upload = multer({ dest: 'public/uploads/' });
 var db = require('./data/students.json');
 
 var app = express();
 var port = 3000;
+var imagePath = "";
 
 app.use(session({
     secret: "Express session secret!"
@@ -13,12 +17,21 @@ app.use(session({
 app.use(bodyParser.urlencoded());
 
 app.use(function(req, res, next) {
-    console.log(req.method + " " + req.protocol + "://" + req.hostname + ":" + port + req.originalUrl);
-    console.log("Session ID: " + req.session.id);
-    console.log(db[0]);
-    console.log(req.body);
+    // console.log(req.method + " " + req.protocol + "://" + req.hostname + ":" + port + req.originalUrl);
+    // console.log("Session ID: " + req.session.id);
+    // console.log(db[0]);
+    // console.log(req.body);
     next();
 });
+
+app.use(function(req, res, next) {
+    if(req.session.login != true && req.originalUrl.indexOf('/welcome.html') != -1) {
+        res.redirect('/login');
+    }
+    next();
+});
+
+app.use(express.static('public'));
 
 app.get('/test', function(req, res) {
     if(req.session.counter == undefined) {
@@ -35,7 +48,10 @@ app.get('/login', function(req, res) {
     res.sendfile('public/index.html');
 })
 
-app.post('/auth', function (req, res) {
+app.post('/auth', upload.single('avatar'), function (req, res) {
+    var index = req.file.path.indexOf("/")
+    imagePath = req.file.path.substring(index+1, req.file.path.length);
+    console.log(imagePath);
     for(var i=0; i<db.length; i++) {
         if(req.body.email === db[i].email && req.body.password === db[i].password) {
             req.session.login = true;
@@ -47,7 +63,8 @@ app.post('/auth', function (req, res) {
 
 app.get('/user', function(req, res) {
     if(req.session.login == true) {
-        res.send("Welcome " + req.session.studentName + ". Do you want to <a href='/logout'>logout</a>?");
+        res.send("Welcome " + req.session.studentName + ". Do you want to <a href='/logout'>logout</a>? <img src='"+imagePath+"'/>");
+        //res.sendfile('public/welcome.html');
     }
     else {
         res.send("You are blocked :(");
@@ -57,6 +74,18 @@ app.get('/user', function(req, res) {
 app.get('/logout', function (req, res) {
     req.session.destroy();
     res.redirect('/login');
+});
+
+app.get('/admin', function (req, res) {
+    var filename = __dirname + '/data/students.json';
+
+    fs.readFile(filename, 'utf8', function(err, data) {
+        var json = JSON.parse(data);
+        json.push(req.query);
+        fs.writeFile(filename, JSON.stringify(json), 'utf8', function () {
+            res.end();
+        });
+    });
 });
 
 app.listen(port);
