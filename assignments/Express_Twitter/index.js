11d405f2a87b06937220b7c7d0b5662b1d153b23
Arkesh Jaiswal <arkesh@attainu.com>
2019-06-10 17:46:13 +0530

    Express_Twitter

---
+++ assignments/Express_Twitter/index.js
@@ -0,0 +1,31 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var app = express();
+
+var tweets = [
+    {
+        name: "digitizer",
+        tweet: "This is a cool piece of information."
+    },
+    {
+        name: "M0SH",
+        tweet: "@digitizer I agree."
+    }
+];
+
+app.use(bodyParser.urlencoded());
+
+app.use(express.static('public'));
+
+app.get('/getTweets', function(req, res) {
+    res.json(tweets);
+});
+
+app.post('/', function(req, res) {
+    console.log(req.body);
+    tweets.push(req.body); // update db
+    res.redirect('/')
+})
+
+app.listen(3000);
